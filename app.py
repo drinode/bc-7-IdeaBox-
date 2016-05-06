ff95fd613d70fb246df1cca8ee24e3a4f8a9b961
Emmah Kimari <emalindahkimari@gmail.com>
2016-05-06 10:04:21 +0300

    Refractored code base by deleting unwanted files

--- app.py
+++ app.py
@@ -16,41 +16,27 @@ from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
 from playhouse.sqlite_ext import *
 
 
-# Blog configuration values.
 
-# You may consider using a one-way hash to generate the password, and then
 
+
+#My demo login password.
 ADMIN_PASSWORD = 'secret'
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# The playhouse.flask_utils.FlaskDB object accepts database URL configuration.
+
 DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
 DEBUG = False
 
-# The secret key is used internally by Flask to encrypt session data stored
-# in cookies. Make this unique for your app.
-SECRET_KEY = 'shhh, secret!'
 
-# This is used by micawber, which will attempt to generate rich media
-# embedded objects with maxwidth=800.
+SECRET_KEY = 'shhh, secret!'
 SITE_WIDTH = 800
 
 
-# Create a Flask WSGI app and configure it using values from the module.
+#Creating my app
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-# FlaskDB is a wrapper for a peewee database that sets up pre/post-request
-# hooks for managing database connections.
 flask_db = FlaskDB(app)
-
-# The `database` is the actual peewee database, as opposed to flask_db which is
-# the wrapper.
 database = flask_db.database
-
-# Configure micawber with the default OEmbed providers (YouTube, Flickr, etc).
-# We'll use a simple in-memory cache so that multiple requests for the same
-# video don't require multiple network requests.
 oembed_providers = bootstrap_basic(OEmbedCache())
 
 
@@ -63,11 +49,6 @@ class Entry(flask_db.Model):
 
     @property
     def html_content(self):
-        """
-        Generate HTML representation of the markdown-formatted blog entry,
-        and also convert any media URLs into rich media objects such as video
-        players or images.
-        """
         hilite = CodeHiliteExtension(linenums=False, css_class='highlight')
         extras = ExtraExtension()
         markdown_content = markdown(self.content, extensions=[hilite, extras])
@@ -79,19 +60,13 @@ class Entry(flask_db.Model):
         return Markup(oembed_content)
 
     def save(self, *args, **kwargs):
-        # Generate a URL-friendly representation of the entry's title.
         if not self.slug:
             self.slug = re.sub('[^\w]+', '-', self.title.lower()).strip('-')
         ret = super(Entry, self).save(*args, **kwargs)
-
-        # Store search content.
         self.update_search_index()
         return ret
 
     def update_search_index(self):
-        # Create a row in the FTSEntry table with the post content. This will
-        # allow us to use SQLite's awesome full-text search extension to
-        # search our entries.
         try:
             fts_entry = FTSEntry.get(FTSEntry.entry_id == self.id)
         except FTSEntry.DoesNotExist:
@@ -114,14 +89,11 @@ class Entry(flask_db.Model):
     def search(cls, query):
         words = [word.strip() for word in query.split() if word.strip()]
         if not words:
-            # Return an empty query.
             return Entry.select().where(Entry.id == 0)
         else:
             search = ' '.join(words)
 
-        # Query the full-text search index for entries matching the given
-        # search query, then join the actual Entry data on the matching
-        # search result.
+        
         return (FTSEntry
                 .select(
                     FTSEntry,
@@ -153,8 +125,6 @@ def login():
     next_url = request.args.get('next') or request.form.get('next')
     if request.method == 'POST' and request.form.get('password'):
         password = request.form.get('password')
-        # TODO: If using a one-way hash, you would also hash the user-submitted
-        # password and do the comparison on the hashed versions.
         if password == app.config['ADMIN_PASSWORD']:
             session['logged_in'] = True
             session.permanent = True  # Use cookie to store session.
@@ -178,11 +148,6 @@ def index():
         query = Entry.search(search_query)
     else:
         query = Entry.public().order_by(Entry.timestamp.desc())
-
-    # The `object_list` helper will take a base query and then handle
-    # paginating the results if there are more than 20. For more info see
-    # the docs:
-    # http://docs.peewee-orm.com/en/latest/peewee/playhouse.html#object_list
     return object_list(
         'index.html',
         query,
@@ -245,11 +210,7 @@ def edit(slug):
 
 @app.template_filter('clean_querystring')
 def clean_querystring(request_args, *keys_to_remove, **new_values):
-    # We'll use this template filter in the pagination include. This filter
-    # will take the current URL and allow us to preserve the arguments in the
-    # querystring while replacing any that we need to overwrite. For instance
-    # if your URL is /?q=search+query&page=2 and we want to preserve the search
-    # term but make a link to page 3, this filter will allow us to do that.
+    
     querystring = dict((key, value) for key, value in request_args.items())
     for key in keys_to_remove:
         querystring.pop(key, None)
